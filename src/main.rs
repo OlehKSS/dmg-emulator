218c888b64bfe2303c231c558ece002de633a316
Oleh Kozynets <oleh.kozynets@gmail.com>
2025-04-09 00:13:23 +0200

    Create a ROM reader.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::process;
+
+use dmg01emu::Cartridge;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Provide a ROM file...");
+        process::exit(1);
+    }
+
+    let rom_file = &args[1];
+
+    println!("Reading {rom_file}");
+
+    if let Err(e) = Cartridge::read(&rom_file) {
+        eprintln!("Error reading ROM {e}");
+        process::exit(1);
+    }
 }
